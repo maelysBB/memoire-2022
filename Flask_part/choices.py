a829eedc6030d4ac1b8857c3a651df35f65e755c
RolaMonzer <rola.monzer95@gmail.com>
2022-10-14 19:57:01 +0200

    mise a jour

--- Flask_part/choices.py
+++ Flask_part/choices.py
@@ -12,7 +12,7 @@ category = {
          },
         "bank" :
             {"name": "Banque",
-            "category" : ["Banque Postale", "BNP Paribas", "BRED/Banque populaire", "Caisse d'Epargne", "CIC", "Crédit Agricole", "Crédit Mutuel", "HSBC", "LCL", "Société Générale"] 
+            "category" : ["Banque Postale", "BNP Paribas", "BRED/Banque populaire", "Caisse d"Epargne", "CIC", "Crédit Agricole", "Crédit Mutuel", "HSBC", "LCL", "Société Générale"] 
             },
         "carpool" : 
         {"name": "Espace de covoiturage",
@@ -39,7 +39,47 @@ category = {
         {"name": "Commerce",
          "category" : ["Achat et entretien voitures", "Agence immobilière", "Boucher", "Boulangerie/Pâtisserie", 
          "Café", "Coiffeur", "Opticien", "Supermarché/Epicerie", "Vêtements/Chaussures"]
-         }         
+         },
+        "allotments" :
+        {"name": "Jardins familiaux",
+         "category" : ["garden", "Autre", "potager", "association"]
+         },
+        "bicycle_parking" :
+        {"name": "Parking à vélos",
+         "category" : ["prive", "libre acces", "Missing"]
+         },
+        "education" : 
+        {"name": "Education",
+         "category" : ["kindergarten", "school", "university", "college"]
+         },
+        "fire_hydrant" :
+        {"name": "Equipement de lutte contre incendie",
+         "category" : ["utiliser_que_par_les_pompiers", "utiliser_par_tous"]
+         },
+        "historic" :
+        {"name": "Historique",
+         "category" : ["memorial", "nature", "man-made_waterbody", "place_of_worship",
+         "monument", "tomb", "castle", "ruins", "Autre", "archaeological_site", "building", 
+         "industrial ", "military", "vehicle", "scientific"]
+         },
+        "hosting" :
+        {"name": "Hébergement touristique",
+         "category" : ["hotel", "guest_house", "outdoor", "Autre"]
+         },
+        "internet_access" :
+        {"name": "Point d"accès internet",
+         "category" : ["wlan", "terminal", "Autre", "wired", "service_provider"] 
+         },
+        "parking" :
+        {"name": "Parking",
+         "category" : ["Missing", "ouvrage", "enclos_en_surface"] 
+         },
+        "sports" : 
+        {"name": "Sportif",
+         "category" : ["sports_centre", "pitch", "horse_riding", "swimming_pool",
+         "golf_course", "fitness_station", "recreation_ground", "track",
+         "stadium", "grass", "Autre"]
+         }
             }
 
 total_number = {} #list the number of subcategory for each type (charging_station, healthcare etc.)
--- Flask_part/converter.py
+++ Flask_part/converter.py
@@ -16,7 +16,15 @@ item_translator = { "healthcare0" : "Hopital", "healthcare1": "Pharmacie", "heal
                     "carpool0": "Aire de covoiturage", "carpool1": "Parking", "cemetery": "cemetery", "cinema0": "MK2", "cinema1": "Pathé Gaumont", "cinema2": "UGC", "cycleway": "cycleway", "library0": "Bibliothèque", "library1": "Médiathèque", "library2": "Dépôt à livres", "playground": "playground", 
                     "recycling": "recycling", "shop_craft_office0": "Achat et entretien voitures", "shop_craft_office1": "Agence immobilière", "shop_craft_office2": "Boucher", 
                     "shop_craft_office3": "Boulanger/Pâtisserie", "shop_craft_office4": "Café", "shop_craft_office5": "Coiffeur", 
-                    "shop_craft_office6": "Opticien", "shop_craft_office7": "Supermarché/Epicerie", "shop_craft_office8": "Vêtements/Chaussures"}
+                    "shop_craft_office6": "Opticien", "shop_craft_office7": "Supermarché/Epicerie", "shop_craft_office8": "Vêtements/Chaussures",
+                    "allotments0": "garden", "allotments1": "Autre", "allotments2": "potager", "allotments3": "association", "bicycle_parking0": "prive", "bicycle_parking1": "libre acces", "bicycle_parking2": "Missing",
+                    "education0": "kindergarten", "education1": "school", "education2" : "university","education3": "college", "fire_hydrant0": "utiliser_que_par_les_pompiers", "fire_hydrant1": "utiliser_par_tous",
+                    "historic0":"memorial", "historic1": "nature", "historic2": "man-made_waterbody", "historic3": "place_of_worship", "historic4": "monument", "historic5": "tomb", "historic6": "castle","historic7": "ruins",
+                    "historic8": "Autre", "historic9": "archaeological_site", "historic10": "building", "historic11": "industrial ", "historic12": "military", "historic13": "vehicle", "historic14": "scientific",
+                    "hosting0": "hotel", "hosting1": "guest_house", "hosting2": "outdoor", "hosting3": "Autre", "internet_access0": "wlan", "internet_access1": "terminal", "internet_access2": "Autre",
+                    "internet_access3": "wired", "internet_access4": "service_provider", "parking0": "Missing", "parking1": "ouvrage", "parking2": "enclos_en_surface", "sports0": "sports_centre", "sports1": "pitch",
+                    "sports2": "horse_riding", "sports3": "swimming_pool", "sports4": "golf_course", "sports5": "fitness_station", "sports6": "recreation_ground", "sports7": "track", "sports8": "stadium", "sports9": "grass", "sports10": "Autre"
+                    }
  
 total_number = {}
 for key, value in category.items() :
