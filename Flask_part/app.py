023d0a57e25ac25466aaff3b5e05ded826e367de
yasmoune <112896087+yasmoune@users.noreply.github.com>
2022-11-25 09:27:26 +0100

    Add files via upload

--- Flask_part/app.py
+++ Flask_part/app.py
@@ -57,12 +57,14 @@ def update_map_api():
     start_coords = (48.7, 2.3360300)
     folium_map = folium.Map(location=start_coords, zoom_start =10)
     checkboxes = data["checkboxes"]
-    inputs =  data["inputs"]    
+    inputs =  data["inputs"]
+    distance= data['distance']
+    url = f"https://b236dd3dc6bb4948883eec9e0dc644d0.us-central1.gcp.cloud.es.io/area{distance}/_search"
     free_field = {}
     if len(inputs) > 0 :
         for key, value in inputs.items():
             free_field[key.replace("name_","")] = value
-    result = test(checkboxes, free_field).to_json()
+    result = test(url, checkboxes, free_field).to_json()
     if len(checkboxes)==0:
         return folium_map._repr_html_()
     return jsonify(result)
@@ -72,14 +74,10 @@ def update_map_api():
 
 @app.route('/app2')
 def app2():
+    list_of_choices =  category
     return render_template(
-        'index_app2.html',
-        data=[{'banque': 0}, {'banque': 1}, {'banque': 2}, {'banque': 3}, {'banque': 4}, {'banque': 5}, {'banque': 6}, {'banque': 7}, {'banque': 8}, {'banque': 9}, {'banque': 10}],
-        data1=[{'santé': 0}, {'santé': 1}, {'santé': 2}, {'santé': 3}, {'santé': 4}, {'santé': 5}, {'santé': 6}, {'santé': 7}, {'santé': 8}, {'santé': 9}, {'santé': 10}],
-        data2=[{'cimetière': 0}, {'cimetière': 1}, {'cimetière': 2}, {'cimetière': 3}, {'cimetière': 4}, {'cimetière': 5}, {'cimetière': 6}, {'cimetière': 7}, {'cimetière': 8}, {'cimetière': 9}, {'cimetière': 10}],
-        data3=[{'cinéma': 0}, {'cinéma': 1}, {'cinéma': 2}, {'cinéma': 3}, {'cinéma': 4}, {'cinéma': 5}, {'cinéma': 6}, {'cinéma': 7}, {'cinéma': 8}, {'cinéma': 9}, {'cinéma': 10}],
-        data4=[{'jardin': 0}, {'jardin': 1}, {'jardin': 2}, {'jardin': 3}, {'jardin': 4}, {'jardin': 5}, {'jardin': 6}, {'jardin': 7}, {'jardin': 8}, {'jardin': 9}, {'jardin': 10}])
-
+        'index_app2.html', list_of_choices = list_of_choices)
+    
 @app.route('/predict_app2', methods=['GET', 'POST'])
 def predict_app2():
     #Extraction des informations saisies par l'utilisateur
@@ -87,10 +85,11 @@ def predict_app2():
     input_data=list(map(int, input_data))
     input_values = [x for x in input_data]
     arr_val = np.array(input_values)
+    print(input_data)
 
     #Préparation des données polygones 
     data = pd.read_csv("polygon_radius_complete.csv")
-    sample = data[data["Nb lieux"] != 0].loc[:, "banque" : "jardin"]
+    sample = data[data["Nb lieux"] != 0].loc[:, "shop_craft_office" : "fuel"]
 
     #Application de la fonction qui calcule la différence entre chaque polygone et les réponses de l'utilisateur
     np_result = np.apply_along_axis(lambda x : difference(input_values, x), 1, sample.to_numpy())
@@ -109,18 +108,28 @@ def predict_app2():
     carte=folium.Map(
         location=[48.866667, 2.3333]
     )
-
     #Ajout de cercles et de popups pour chaque polygone
     for i in range(0,len(top)):
         if top['diff'].iloc[i]==0:
             popup1 = """
     Ceci est un match parfait !<br>
-    Nombre de <b>banque(s)</b> : <b>%s</b><br>
-    Nombre de <b>santé(s)</b> : <b>%s</b><br>
-    Nombre de <b>cimetière(s)</b> : <b>%s</b><br>
-    Nombre de <b>cinéma(s)</b> : <b>%d</b><br>
-    Nombre de <b>jardin(s)</b> : <b>%d</b><br>
-    """ % (top['banque'].iloc[i], top['santé'].iloc[i], top['cimetière'].iloc[i],top['cinéma'].iloc[i],top['jardin'].iloc[i])
+    Dans cette zone,se trouvent <b>%s</b> commerce(s), <b>%s</b> restaurant(s),<br> 
+    <b>%s</b> cinéma(s), <b>%s</b> bibliothèque(s), <b>%s</b> aire(s) de jeux,<br> 
+    <b>%s</b> installation(s) sportive(s), <b>%s</b> jardin(s), <b>%s</b> lieu(x) historique(s),<br> 
+    <b>%s</b> hébergement(s), <b>%s</b> école(s), <b>%s</b>  lieu(x) de santé,<br> 
+    <b>%s</b> banque(s), <b>%s</b> service(s) public(s), <b>%s</b> toilette(s),<br> 
+    <b>%s</b> station(s) d'eau potable, <b>%s</b> défibrillateur(s),<br> 
+    <b>%s</b> équipement(s) anti-incendie, <b>%s</b> station(s) de covoiturage, <br>
+    <b>%s</b> parking(s), <b>%s</b> parking(s) vélo, <b>%s</b> borne(s) de chargement, <br>
+    <b>%s</b> station(s) de taxi et <b>%s</b> station(s) essence
+    """ % (top['shop_craft_office'].iloc[i], top['cinema'].iloc[i], top['restaurant'].iloc[i],
+    top['library'].iloc[i], top['playground'].iloc[i], top['sports'].iloc[i],top['allotments'].iloc[i], 
+    top['historic'].iloc[i], top['hosting'].iloc[i], top['education'].iloc[i],
+    top['healthcare'].iloc[i], top['bank'].iloc[i], top['public_service'].iloc[i],
+    top['toilets'].iloc[i], top['drinking_water'].iloc[i], top['aed'].iloc[i],
+    top['fire_hydrant'].iloc[i], top['carpool'].iloc[i], top['parking'].iloc[i],
+    top['bicycle_parking'].iloc[i], top['charging_station'].iloc[i], top['taxi'].iloc[i],
+    top['fuel'].iloc[i])
             folium.Circle(location=[top['lat'].iloc[i], top['lon'].iloc[i]],
                     radius=1000,
                     tooltip=popup1,
@@ -130,12 +139,23 @@ def predict_app2():
         else:
             popup2 = """
     Ce n'est pas un match parfait !<br>
-    Nombre de <b>banque(s)</b> : <b>%s</b><br>
-    Nombre de <b>santé(s)</b> : <b>%s</b><br>
-    Nombre de <b>cimetière(s)</b> : <b>%s</b><br>
-    Nombre de <b>cinéma(s)</b> : <b>%d</b><br>
-    Nombre de <b>jardin(s)</b> : <b>%d</b><br>
-    """ % (top['banque'].iloc[i], top['santé'].iloc[i], top['cimetière'].iloc[i],top['cinéma'].iloc[i],top['jardin'].iloc[i])
+        Dans cette zone,se trouvent <b>%s</b> commerce(s), <b>%s</b> restaurant(s),<br> 
+    <b>%s</b> cinéma(s), <b>%s</b> bibliothèque(s), <b>%s</b> aire(s) de jeux,<br> 
+    <b>%s</b> installation(s) sportive(s), <b>%s</b> jardin(s), <b>%s</b> lieu(x) historique(s),<br> 
+    <b>%s</b> hébergement(s), <b>%s</b> école(s), <b>%s</b>  lieu(x) de santé,<br> 
+    <b>%s</b> banque(s), <b>%s</b> service(s) public(s), <b>%s</b> toilette(s),<br> 
+    <b>%s</b> station(s) d'eau potable, <b>%s</b> défibrillateur(s),<br> 
+    <b>%s</b> équipement(s) anti-incendie, <b>%s</b> station(s) de covoiturage, <br>
+    <b>%s</b> parking(s), <b>%s</b> parking(s) vélo, <b>%s</b> borne(s) de chargement, <br>
+    <b>%s</b> station(s) de taxi et <b>%s</b> station(s) essence
+    """ % (top['shop_craft_office'].iloc[i], top['cinema'].iloc[i], top['restaurant'].iloc[i],
+    top['library'].iloc[i], top['playground'].iloc[i], top['sports'].iloc[i],top['allotments'].iloc[i], 
+    top['historic'].iloc[i], top['hosting'].iloc[i], top['education'].iloc[i],
+    top['healthcare'].iloc[i], top['bank'].iloc[i], top['public_service'].iloc[i],
+    top['toilets'].iloc[i], top['drinking_water'].iloc[i], top['aed'].iloc[i],
+    top['fire_hydrant'].iloc[i], top['carpool'].iloc[i], top['parking'].iloc[i],
+    top['bicycle_parking'].iloc[i], top['charging_station'].iloc[i], top['taxi'].iloc[i],
+    top['fuel'].iloc[i])
             folium.Circle(location=[top['lat'].iloc[i], top['lon'].iloc[i]],
                     radius=1000,
                     tooltip=popup2,
--- Flask_part/choices.py
+++ Flask_part/choices.py
@@ -1,68 +1,43 @@
 category = {
-        "healthcare" : 
-        {"name": "Etablissement de santé",                                    # list all possible categories
-         "category" : ["Clinique", "Pharmacie", "Hopitaux"],
-         "title":"oui"
-         },
-        "charging_station" :
-            {"name": "Bornes de chargement", 
-            },
-        "aed" : 
-        {"name": "Défibrillateur",
-         "category" : ["Intérieur", "Extérieur"]
-         },
-        "bank" :
-            {"name": "Banque",
-            "category" : ["Banque Postale", "BNP Paribas", "BRED/Banque populaire", "Caisse d'Epargne", "CIC", "Crédit Agricole", "Crédit Mutuel", "HSBC", "LCL", "Société Générale"] 
-            },
-        "carpool" : 
-        {"name": "Espace de covoiturage",
-         "category" : ["Aire de covoiturage", "Parking"]
-         },
-        "cemetery" :
-            {"name": "Cimetière", 
-            "title":"oui"
-            },
-        "cinema" : 
-        {"name": "Cinéma",
-         "category" : ["MK2", "Pathé Gaumont", "UGC"],
-         "title":"oui"
-         },
-        "cycleway" :
-            {"name": "Piste cyclable",}, 
-        "library" : 
-        {"name": "Bibliothèque",
-         "category" : ["Bibliothèque", "Médiathèque", "Dépôt à livres"],
-         "title":"oui"
-         },
-        "playground" :
-        {"name": "Terrain de jeu",
-        "title":"oui"}, 
-        "recycling" :
-        {"name": "Recyclage",}, 
         "shop_craft_office" : 
         {"name": "Commerce",
          "category" : ["Achat et entretien voitures", "Agence immobilière", "Boucher", "Boulangerie/Pâtisserie", 
          "Café", "Coiffeur", "Opticien", "Supermarché/Epicerie", "Vêtements/Chaussures"],
+         "title":"oui",
+         "groupe":"Commerces et restaurants"
+         },
+        "restaurant":
+        {"name": "Restaurants",
+        "category" : ["Bar ou Pub", "Café", "Glacier", "Foire alimentaire", "Restaurant Fast Food", 
+        "Restaurant Français", "Restaurant Régional", "Restaurant Italien", "Restaurant Japonais", "Restaurant Indien", "Autre restaurant"],
+        "title":"oui"
+         },
+
+        "cinema" : 
+        {"name": "Cinéma",
+         "category" : ["MK2", "Pathé Gaumont", "UGC"],
+         "title":"oui",
+         "groupe":"Lieux de divertissement et tourisme"
+         },
+         "library" : 
+        {"name": "Bibliothèque",
+         "category" : ["Bibliothèque", "Médiathèque", "Dépôt à livres"],
+         "title":"oui"         },
+        "playground" :
+        {"name": "Terrain de jeu",
+        "title":"oui"}
+        , 
+        "sports" : 
+        {"name": "Aménagement sportif",
+         "category" : ["Aire de jeux", "Centre d'équitation", "Complexe sportif",
+         "Equipement de fitness ou de gymnastique", "Piscine", "Piste d'athlétisme", "Stade",
+         "Terrain de golf", "Terrain de sport", "Autre"],
          "title":"oui"
          },
         "allotments" :
         {"name": "Jardins familiaux",
          "category" : ["Jardin", "Potager", "Géré par une association", "Autre"], 
          "title":"oui"
-         },
-        "bicycle_parking" :
-        {"name": "Parking à vélos",
-         "category" : ["Privé", "Libre accès"]
-         },
-        "education" : 
-        {"name": "Education",
-         "category" : ["Crèche", "Ecole primaire ou secondaire", "Enseignement supérieur hors université", "Université"],
-         "title":"oui"
-         },
-        "fire_hydrant" :
-        {"name": "Equipement de lutte contre incendie",
-         "category" : ["Utiliser par tous", "Utiliser que par les pompiers"]
          },
         "historic" :
         {"name": "Historique",
@@ -74,53 +49,68 @@ category = {
         {"name": "Hébergement touristique",
          "category" : ["Hôtel", "Maison d'hôte", "Espaces extérieur", "Autre"]
          },
-        "internet_access" :
-        {"name": "Point d'accès internet",
-         "category" : ["Wifi", "Connection par câble", "Fournisseur de service", "Autre"] 
+
+        "education" : 
+        {"name": "Education",
+         "category" : ["Crèche", "Ecole primaire ou secondaire", "Enseignement supérieur hors université", "Université"],
+         "title":"oui",
+         "groupe":"Enseignement et santé"
          },
-        "parking" :
-        {"name": "Parking",
-         "category" : ["Ouvrage", "Enclos en surface"] 
-         },
-        "sports" : 
-        {"name": "Sportif",
-         "category" : ["Aire de jeux", "Centre d'équitation", "Complexe sportif",
-         "Equipement de fitness ou de gymnastique", "Piscine", "Piste d'athlétisme", "Stade",
-         "Terrain de golf", "Terrain de sport", "Autre"],
+        "healthcare" : 
+        {"name": "Etablissement de santé",                                    # list all possible categories
+         "category" : ["Clinique", "Pharmacie", "Hopitaux"],
          "title":"oui"
          },
-        "drinking_water":
-        {"name": "Eau Potable",
-         "category" : ["Gratuit", "Payant"]
+
+        "bank" :
+        {"name": "Banque",
+        "category" : ["Banque Postale", "BNP Paribas", "BRED/Banque populaire", "Caisse d'Epargne", "CIC", "Crédit Agricole", "Crédit Mutuel", "HSBC", "LCL", "Société Générale"] 
+        },
+        "public_service":
+        {"name": "Services Publiques",
+         "category" : ["Mairie ou Hôtel de ville", "Organisme gouvernemental", "Service de conseil", "Diplomatique", "Autre"],
+         "title":"oui",
+         "groupe":"Services"
          },
         "toilets":
         {"name": "Toilettes",
          "category" : ["Gratuit", "Payant"]
          },
+        "drinking_water":
+        {"name": "Eau Potable",
+         "category" : ["Gratuit", "Payant"]
+         },
+        "aed" : 
+        {"name": "Défibrillateur"
+         },
+        "fire_hydrant" :
+        {"name": "Equipement de lutte contre incendie",
+         "category" : ["Utilisation par tous", "Utilisation par les pompiers"]
+         },   
+                
+        "carpool" : 
+        {"name": "Espace de covoiturage",
+         "category" : ["Aire de covoiturage", "Parking"],
+         "groupe":"Transport"
+         },
+        "parking" :
+        {"name": "Parking",
+         "category" : ["Ouvrage", "Enclos en surface"]
+         },
+        "bicycle_parking" :
+        {"name": "Parking à vélos",
+         "category" : ["Privé", "Libre accès"]
+         },
+        "charging_station" :
+        {"name": "Bornes de chargement"
+        },
         "taxi":
-        {"name": "Taxi",
+        {"name": "Taxi"
          },
         "fuel":
-        {"name": "Station électrique",
-         },
-        "power_supports":
-        {"name": "Support du courant",
-         "category" : [ "Pylône électrique", "Poteau électrique"]
-         },
-        "restaurant":
-        {"name": "Restaurants",
-         "category" : ["Bar ou Pub", "Café", "Glacier", "Foire alimentaire", "Restaurant", "Restaurant Fast Food", 
-         "Restaurant Français", "Restaurant Régional", "Restaurant Italien", "Restaurant Japonais", "Restaurant Indien"],
-         "title":"oui"
-         },
-        "public_service":
-        {"name": "Services Publiques",
-         "category" : ["Mairie ou Hôtel de ville", "Organisme gouvernemental", "Service de conseil", "Diplomatique", "Autre"],
-         "title":"oui"
+        {"name": "Station essence"
          }
-         
-         
-            }
+         }
 
 total_number = {} #list the number of subcategory for each type (charging_station, healthcare etc.)
 name_cat = []
