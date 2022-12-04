8e2af146f7b93b3a12ae459183cd1a69e56e83ac
maelysBB <65898346+maelysBB@users.noreply.github.com>
2022-12-04 12:59:11 +0100

    Script updates

--- Flask_part/static/style/js/script.js
+++ Flask_part/static/style/js/script.js
@@ -1,40 +1,8 @@
-// function toggle(source) {
-//     checkboxes = document.getElementsByName('health');
-//     for(var i=0, n=checkboxes.length;i<n;i++) {
-//       checkboxes[i].checked = source.checked;
-//     }
-//     }
-
-//     $('#thisdiv').load(document.URL +  ' #thisdiv');
-
-
-// function collapse(coll) {
-//   // console.log("mae")
-//   var coll = document.getElementsByClassName("collapsible");
-
-//   var i;
-//   for (i = 0; i < coll.length; i++) {
-//     coll[i].addEventListener("click", function() {
-//       console.log("hi")
-//       this.classList.toggle("active");
-//       var content = document.getElementsByClassName("content");
-//       console.log(content)
-//       for (i = 0; i < content.length; i++) {
-//         if (content[i].style.display === "block") {
-//           content[i].style.display = "none";
-//         } else {
-//           content[i].style.display = "block";
-//       }}
-//     });
-//   }
-// y
-
 function collapse(event) { 
   console.log(event)
   event.classList.toggle("active");
   id = event.id.split("-")[1]
   var content = document.getElementsByClassName(`content ${id}`);
-  // console.log()
   for (i = 0; i < content.length; i++) {
     if (content[i].style.display === "block") {
       content[i].style.display = "none";
@@ -44,14 +12,16 @@ function collapse(event) {
 
 }
 
-// window.onload = function() {
-//   let myiFrame = document.getElementById("map");
-//   let doc = myiFrame.contentDocument;
-//   doc.body.innerHTML = doc.body.innerHTML + '<style>span {display: none!important}</style>';
-// }
 
+var values = [500,1000,5000,10000];
 
+function changeSlider(){
+  
+  var slider_vals = ["500 m","1 km","5 km","10 km"];
+    var input = document.getElementById('input');
+    var output = document.getElementById('output');
+    output.innerHTML = slider_vals[input.value];
+    output.style = `margin-left: ${input.value*25 + 10}%; font-weight: 500`
+    console.log(output.style)
 
-// var col = document.getElementsByClassName("collapsible");
-// collapse()
-
+};
